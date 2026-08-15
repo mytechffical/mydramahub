27b8e5694a3d052cde44fb4ed9c3da1a90a51769
mytechffical <husnaina2basofficial@gmail.com>
2026-08-15 22:20:17 +0500

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,5 @@
 import type { MetadataRoute } from "next";
 import { siteUrl } from "@/lib/seo";
 export default function robots(): MetadataRoute.Robots {
-  return { rules: [{ userAgent: "*", allow: "/", disallow: "/admin" }], sitemap: siteUrl("/sitemap.xml") };
+  return { rules: [{ userAgent: "*", allow: "/", disallow: ["/admin", "/api"] }], sitemap: siteUrl("/sitemap.xml") };
 }
